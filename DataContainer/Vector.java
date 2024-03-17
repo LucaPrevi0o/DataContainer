2687865cec277b1f3a81a9aee0bb740e00fba4f3
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-03-17 09:41:04 +0100

    modified:   DataContainer/Vector.java -> added comments

--- DataContainer/Vector.java
+++ DataContainer/Vector.java
@@ -4,22 +4,22 @@ public class Vector<T> {
 
     private T value=null; //stored value
     private Vector<T> next=null; //next element in vector
-    private Vector(T e) { this.value=e; } //initializer compiler
+    private Vector(T e) { this.value=e; } //private initializer compiler
 
-    public Vector<T> push(T e) {
+    public Vector<T> push(T e) { //add new element in last position
 
-        Vector<T> i;
-        for (i=this; i.next!=null; i=i.next);
-        i.next=new Vector<T>(e);
-        return this;
+        Vector<T> i; //iterator element
+        for (i=this; i.next!=null; i=i.next); //scroll the entire list
+        i.next=new Vector<T>(e); //add new element as last
+        return this; //return this (for chaining)
     }
 
-    public T get(int index) throws ArrayIndexOutOfBoundsException {
+    public T get(int index) throws ArrayIndexOutOfBoundsException { //return element in specified position (throws exception)
 
-        Vector<T> i=this;
-        for (int a=0; a<index && i!=null; a++) i=i.next;
-        if (i==null) throw new ArrayIndexOutOfBoundsException();
-        return i.value;
+        Vector<T> i=this; //iterator element
+        for (int a=0; a<index && i!=null; a++) i=i.next; //scroll the list until index or last element
+        if (i==null) throw new ArrayIndexOutOfBoundsException(); //throw exception if null element
+        return i.value; //return element in index position otherwise
     }
 
     public Vector() {} //empty constructor
