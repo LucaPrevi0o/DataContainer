72ed3e84953f4e2bbd5a04daaae822d1f2455270
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-03-17 09:34:30 +0100

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+import DataContainer.*;
 
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Vector<Integer> b=new Vector<Integer>();
+        b.push(4);
+    }
+}
