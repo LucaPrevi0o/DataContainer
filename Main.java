de8f02bda51be6c96523169ba354d84088cdc50e
LUCA PREVIATI <luca01.previati@edu.unife.it>
2024-03-19 12:19:48 +0100

    updated classes

--- Main.java
+++ Main.java
@@ -6,9 +6,10 @@ public class Main {
         
         Vector<Integer> b=new Vector<Integer>();
         for (int i=0; i<6; i++) b.push(i);
-        Vector<Integer> b1=b.copy().reverse().push(12);
+        Vector<Integer> b1=b.reverse().push(12);
         System.out.println(b);
         System.out.println(b1.remove(3));
+        System.out.println(b1.reverse()+" - "+b1+" - "+b1.flip()+" - "+b1);
         b.clear();
         b.push(5);
         System.out.println(b);
