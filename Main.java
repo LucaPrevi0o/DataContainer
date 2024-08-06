3618c7a0ffbe9ffa507f50574f8ba38f5f78cd1d
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-08-06 14:07:48 +0200

    finished vector class (i think)

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
-import DataContainer.*;
+import DataContainer.Vector;
 
 public class Main {
     
     public static void main(String[] args) {
         
-        Vector<Integer> b=new Vector<Integer>();
-        b.push(4);
-        b.push(9);
-        b.push(2);
-        b.add(1, -4);
-        b.push(5);
-        b.append(-3);
-        for (int a=0; a<b.size(); a++) System.out.println(b.get(a));
-        b.set(3, -8);
-        for (int a=0; a<b.size(); a++) System.out.println(b.get(a));
-        System.out.println(b);
+        Vector<Integer> myVector=new Vector<Integer>(10, 9, 8);
+        myVector.push(4);
+        myVector.append(3);
+        myVector.push(2);
+        System.out.println("now testing: "+myVector.last());
+        System.out.println(myVector);
+        for (var i: myVector) System.out.println(i);
+        var c=myVector.reverse().remove(1).pop().set(1, -10).pop().push(-30).shrink(4).cut();
+        System.out.println(c+"\n"+myVector);
     }
 }
